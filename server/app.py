6c180dc794baf604fe7623bb48f1e1a75554c48e
myrinja <myrinjau98@gmail.com>
2023-10-26 14:19:20 +0300

    update all

--- server/app.py
+++ server/app.py
@@ -29,12 +29,38 @@ def bakeries():
         200
     )
     return response
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    name = request.form.get('name')
+    price = request.form.get('price')
+    bakery_id = request.form.get('bakery_id')
 
-@app.route('/bakeries/<int:id>')
+    if not name or not price or not bakery_id:
+        # Handle validation error
+        return make_response(jsonify({'error': 'Missing data'}), 400)
+
+    # Create a new baked good in the database
+    new_baked_good = BakedGood(name=name, price=price, bakery_id=bakery_id)
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    # Return the created baked good as JSON
+    return jsonify({
+        'id': new_baked_good.id,
+        'name': new_baked_good.name,
+        'price': new_baked_good.price,
+        'bakery_id': new_baked_good.bakery_id
+    }), 201
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
 def bakery_by_id(id):
+    new_name = request.form.get('name')
 
     bakery = Bakery.query.filter_by(id=id).first()
     bakery_serialized = bakery.to_dict()
+    
+    bakery.name = new_name
+    db.session.commit()
+
 
     response = make_response(
         bakery_serialized,
@@ -65,6 +91,18 @@ def most_expensive_baked_good():
         200
     )
     return response
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    # Find the baked good by ID
+    baked_good = BakedGood.query.get(id)
+    
+    # Delete the baked good from the database
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    # Return a JSON response confirming the deletion
+    return jsonify({'message': 'Baked good deleted'})
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
